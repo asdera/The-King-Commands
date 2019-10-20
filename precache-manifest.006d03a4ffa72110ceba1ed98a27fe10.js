56a1763f666fbf9cb733e1619c960ece53504706
unknown <poiuytandrew@gmail.com>
2019-10-20 12:28:20 -0400

    Updates

---
+++ precache-manifest.006d03a4ffa72110ceba1ed98a27fe10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58cf6ac48ebf359d0b394181dd3f5789",
+    "url": "/The-King-Commands/index.html"
+  },
+  {
+    "revision": "85e9e1fa4f87351cf800",
+    "url": "/The-King-Commands/static/css/main.ae77683a.chunk.css"
+  },
+  {
+    "revision": "b7011846e9b284f6d295",
+    "url": "/The-King-Commands/static/js/2.7469bf70.chunk.js"
+  },
+  {
+    "revision": "85e9e1fa4f87351cf800",
+    "url": "/The-King-Commands/static/js/main.f2d8eb58.chunk.js"
+  },
+  {
+    "revision": "c413335d2da534d1e70c",
+    "url": "/The-King-Commands/static/js/runtime-main.35a2375f.js"
+  },
+  {
+    "revision": "38c9083462c630b693cb294b885fd2f5",
+    "url": "/The-King-Commands/static/media/backdrop.38c90834.png"
+  }
+]);
